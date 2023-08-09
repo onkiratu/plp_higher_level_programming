5697ac13a6414daae49643ffad6c0b2db2a2ac4c
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-09 21:49:29 +0300

    question 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,9 @@ def uppercase(str):
         #PRINTING UPPERCASE
         if (ord(i) >= 65 and ord(i) <= 90) or (i >= "0" and i <= "9") or (i == " ") or (i == '\n'):
             num = ord(i)
-            print(end=chr(num))
+            print("{}".format(num))
         else:
             #CHANGING LOWERCASE TO UPPERCAS
             num = ord(i) - 32
             num = chr(num)
-            print(end=num)
+            print("{}".format(num))
