ec9465fa0e30af51d9b17bf040972b0409941fed
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-09 21:45:42 +0300

    question 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def uppercase(str):
+    for i in str:
+        #PRINTING UPPERCASE
+        if (ord(i) >= 65 and ord(i) <= 90) or (i >= "0" and i <= "9") or (i == " ") or (i == '\n'):
+            num = ord(i)
+            print(end=chr(num))
+        else:
+            #CHANGING LOWERCASE TO UPPERCAS
+            num = ord(i) - 32
+            num = chr(num)
+            print(end=num)
