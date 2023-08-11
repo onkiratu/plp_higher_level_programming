451fa0bf50256db78f506e3ca80f14880aa696a1
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-11 17:56:17 +0300

    question zero

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-add_0.add(a, b)
+from add_0 import add
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add(a, b):
-    print("{} + {} = {}".format(a, b, (a+b)))
+    print("{} + {} = {}".format(a, b, (a + b)))
 
-if __name__ == "__main__":
+if not  __name__ == "__main__":
+    a = 1
+    b = 2
     add(a, b)
