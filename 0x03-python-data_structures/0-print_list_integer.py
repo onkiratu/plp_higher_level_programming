bc48a3fbbef737afdd900e37bd56aa182b8a85b3
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-14 17:11:20 +0300

    question 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for integers in my_list:
-        integers = "{}".format(integers)
+        integers = "% s" % integers
         print("{}".format(integers))
