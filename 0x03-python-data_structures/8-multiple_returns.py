bc147bfd90f5b60bfbcf3918148432f3041e3531
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-15 22:00:14 +0300

    question 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[0] = None
+
     n = len(sentence)
     first = sentence[0]
     tuple_a = (n, first)
