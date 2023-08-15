f9e5d2a749f8040c2ff31b9219be3479e3864321
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-15 22:10:50 +0300

    question 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
+    if not sentence:
+        return 0, None
 
     n = len(sentence)
     first = sentence[0]
