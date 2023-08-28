12cf31c48ca167398600110b8f9d25b26b7651f9
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-28 19:06:13 +0300

    question 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list[:x]:
+            print(end="{}".format(i))
+            count += 1
+            print()
+    except TypeError:
+        print()
+        return count
