2ad97fef4663b1c9d6c05fd11b012e641c485c45
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-29 05:08:56 +0300

    question 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
