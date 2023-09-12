5472bb37d3bd9271ee56d885becab813925c2898
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-13 00:13:27 +0300

    question 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,17 +1,8 @@
-#!usr/bin/python3
-"""
-module contains a function that returns a JSON rep of an object
-"""
+#!/usr/bin/python3
+"""This module defines a string-to-JSON function"""
 import json
 
+
 def to_json_string(my_obj):
-    """
-    Returns the JSON representation of an object(string).
-
-    Args:
-        my_obj: object to return the rep of
-
-    Return:
-        JSON rep of my_obj
-    """
+    """Returns the JSON representation of a string object"""
     return json.dumps(my_obj)
