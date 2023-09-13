2813f8be36c12dabfab0bd817810ca17314adf1d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-13 14:26:51 +0300

    question 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,9 +4,6 @@
 
 class BaseGeometry:
     """Defines an area method"""
-    def __init__(self):
-        pass
-
     def area(self):
         """Raises an exception"""
         raise Exception("area() is not impleamented")
