4c8555d8238671271fc66606a1e8cb8435951668
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-18 19:09:34 +0300

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """Class inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initilizing class attributes"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        Base.__init__(self, id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(self, id)
 
         if not isinstance(self.__height, int):
             raise TypeError("height must be an integer")
