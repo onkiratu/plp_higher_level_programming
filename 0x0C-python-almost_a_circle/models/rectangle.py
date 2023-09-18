abb80b3b35eb4cbf538853c52018f6319ab34869
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-18 19:16:28 +0300

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,36 +7,36 @@ class Rectangle(Base):
     """Class inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initilizing class attributes"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-        if not isinstance(self.__height, int):
-            raise TypeError("height must be an integer")
-
-        if self.__height <= 0:
-            raise ValueError("height must be > 0")
-
-        if not isinstance(self.__width, int):
-            raise TypeError("width must be > 0")
-
-        if self.__width <= 0:
-            raise ValueError("width must be an integer")
-
-        if not isinstance(self.__x, int):
-            raise TypeError("x must be an integer")
-
-        if not isinstance(self.__y, int):
-            raise TypeError("y must be an integer")
-
-        if self.__x < 0:
-            raise ValueError("x must be >= 0")
-
-        if self.__y < 0:
-            raise ValueError("y must be >= 0")
-
+#        if not isinstance(self.__height, int):
+#            raise TypeError("height must be an integer")
+#
+#        if self.__height <= 0:
+#            raise ValueError("height must be > 0")
+#
+#        if not isinstance(self.__width, int):
+#            raise TypeError("width must be > 0")
+#
+#        if self.__width <= 0:
+#            raise ValueError("width must be an integer")
+#
+#        if not isinstance(self.__x, int):
+#            raise TypeError("x must be an integer")
+#
+#        if not isinstance(self.__y, int):
+#            raise TypeError("y must be an integer")
+#
+#        if self.__x < 0:
+#            raise ValueError("x must be >= 0")
+#
+#        if self.__y < 0:
+#            raise ValueError("y must be >= 0")
+#
     @property
     def width(self):
         """returns the value of width"""
