69db3d9b820ea7e55a92c62101ae7c4925dff149
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-07 20:57:38 +0300

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,5 @@
 # Script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 URL="$1"
-curl $URL | grep "Content_Length"
+curl $URL | grep "Content-Length"
+
