4d15c33f112b9128147124d347fcd24ac86622a5
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-07 21:53:49 +0300

    question 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 URL="$1"
-curl -is $URL | grep "Content-Length" | tail -n 1 | awk '{print $2}'
+curl -s $URL | wc -c
