ce89a325e55e86caeabb6598c17e9518c7397e41
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-09 21:57:20 +0300

    question 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL
-curl -d "email: test@gmail.com" "subject: I will always be here for PLD" "$1"
+curl -d "email: test@gmail.com" "$1"
