ce770ad542fee000b3c6356b9fc1ff07003b7bc4
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-12 14:23:32 +0300

    quest 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         data = response.read()
-        print(f"- type: {type(data)}")
-        print(f"- content: {data}")
-        print(f"- utf8 content: {data.decode('utf-8')}")
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode("utf-8")))
