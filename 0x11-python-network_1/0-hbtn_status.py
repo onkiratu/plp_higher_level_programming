7696eded75b10e1c5706d7ae1832ffa67bcb83f0
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-12 11:40:58 +0300

    question 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,7 @@ from urllib.request import urlopen
 url = "https://alx-intranet.hbtn.io/status"
 with urlopen(url) as response:
     data = response.read()
-
-print("Body response:")
-print(f"\t - type: {type(data)}")
-print(f"\t - content: {data}")
-print(f"\t - utf8 content: {data.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t - type: {type(data)}")
+    print(f"\t - content: {data}")
+    print(f"\t - utf8 content: {data.decode('utf-8')}")
