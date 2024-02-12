e603891856606931c46e87f307e3564fc9994901
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-02-12 11:46:55 +0300

    question 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,12 @@ Module contains a script tat fetches https://alx-intranet.hbtn.io/status
 """
 from urllib.request import urlopen
 
-url = "https://alx-intranet.hbtn.io/status"
-with urlopen(url) as response:
-    data = response.read()
-    print("Body response:")
-    print(f"\t - type: {type(data)}")
-    print(f"\t - content: {data}")
-    print(f"\t - utf8 content: {data.decode('utf-8')}")
+if __name__ == "__main__":
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        data = response.read()
+        print("Body response:")
+        print(f"\t - type: {type(data)}")
+        print(f"\t - content: {data}")
+        print(f"\t - utf8 content: {data.decode('utf-8')}")
