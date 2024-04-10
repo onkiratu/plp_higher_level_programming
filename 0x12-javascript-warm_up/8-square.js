c8a97d25594dac755990b65508877ca1beab6aba
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-10 22:48:54 +0300

    question 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv
+if (isNaN(args[2])) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < args[2]; i++) {
+    for (let j = 0; j < args[2]; j++) {
+	  console.log("X");
+	}
+  }
+}
