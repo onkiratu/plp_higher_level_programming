25f627cacf8aecb9d472879b03bfbebeb15bb69c
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-11 18:53:55 +0300

    question 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+}
+
+module.exports = Rectangle;
