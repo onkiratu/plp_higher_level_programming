762d1a7d6babbb2bdd0de4b330fb9352713c182a
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-11 20:19:23 +0300

    question 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const SquareOne = require('./5-square');
+
+class Square extends SquareOne {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        console.log();
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('C');
+        }
+        console.log();
+      }
+    }
+  }
+}
+module.exports = Square;
