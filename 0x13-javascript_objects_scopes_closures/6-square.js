1010530345365dd0a0fc9bce4eda3a193cb14171
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-11 20:22:40 +0300

    question 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -14,7 +14,7 @@ class Square extends SquareOne {
     } else {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
+          process.stdout.write('c');
         }
         console.log();
       }
