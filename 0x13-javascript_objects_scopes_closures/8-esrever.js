0e69892656266d6f577e5f1ee765db35e9481636
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-04-12 05:44:20 +0300

    question 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let myList = [];
+  const myList = [];
   for (let i = 0; i < list.length; i++) {
     myList.unshift(list[i]);
   }
   return myList;
-}
+};
